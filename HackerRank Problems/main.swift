52a7fe30f43dfb71b7a5ce404b63e74f90dc4d03
mekala-vamsi-krishna <vamsikrishnayadav2002@gmail.com>
2025-06-30 17:49:00 +0530

    1. Plus Minus 2. Stair Case

--- HackerRank Problems/main.swift
+++ HackerRank Problems/main.swift
@@ -90,4 +90,58 @@ for i in 0..._2DArray.count - 1 {
 print(left)
 print(right)
 
+func plusMinus(arr: [Int]) -> Void {
+    var positiveCount = 0.0
+    var negativeCount = 0.0
+    var zeroCount = 0.0
+    
+    var arrayCount = Double(arr.count)
+    var result = [Double]()
+    
+    for i in arr {
+        if i > 0 {
+            positiveCount = positiveCount + 1
+        } else if i < 0 {
+            negativeCount = negativeCount + 1
+        } else {
+            zeroCount = zeroCount + 1
+        }
+    }
+    
+    let positiveResult = Double(positiveCount) / Double(arrayCount)
+    let negativeResult = Double(negativeCount) / Double(arrayCount)
+    let zeroResult = Double(zeroCount) / Double(arrayCount)
+    
+    result.append(positiveResult)
+    result.append(negativeResult)
+    result.append(zeroResult)
+    
+    for value in result {
+        print(String(format: "%.6f", value))
+    }
+    
+}
 
+plusMinus(arr: [1,1,0,-1,-1])
+
+func stairCase(n: Int) {
+    guard n > 0 else {
+        print("Number of rows must be greater than 0")
+        return
+    }
+    for i in 1...n {
+        if (n - i) > 0 {
+            for _ in 1...(n - i) {
+                print("", terminator: " ")
+            }
+        }
+        
+        // Inner loop for printing stars
+        for _ in 1...i {
+            print("#", terminator: "")
+        }
+        print(" ", terminator: "\n")
+    }
+}
+
+stairCase(n: 5)
